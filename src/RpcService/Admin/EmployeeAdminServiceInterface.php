e11b73ff64399e783987fd945e12ca59a164423c
pangningyi <1299553744@qq.com>
2025-07-25 16:50:25 +0800

    rpc

--- src/RpcService/Admin/EmployeeAdminServiceInterface.php
+++ src/RpcService/Admin/EmployeeAdminServiceInterface.php
@@ -16,51 +16,45 @@ interface EmployeeAdminServiceInterface
     
     /**
      * 更新员工信息
-     * @param int $corpId 企业ID
      * @param int $employeeId 员工ID
      * @param array $data 员工信息
      * @return array
      */
-    public function update(int $corpId, int $employeeId, array $data): array;
+    public function update(int $employeeId, array $data): array;
     
     /**
      * 禁用员工
-     * @param int $corpId 企业ID
      * @param int $employeeId 员工ID
      * @return array
      */
-    public function disable(int $corpId, int $employeeId): array;
+    public function disable(int $employeeId): array;
     
     /**
      * 启用员工
-     * @param int $corpId 企业ID
      * @param int $employeeId 员工ID
      * @return array
      */
-    public function enable(int $corpId, int $employeeId): array;
+    public function enable(int $employeeId): array;
     
     /**
      * 获取员工详情
-     * @param int $corpId 企业ID
      * @param int $employeeId 员工ID
      * @return array
      */
-    public function info(int $corpId, int $employeeId): array;
+    public function info(int $employeeId): array;
     
     /**
      * 获取企业下的员工列表
-     * @param int $corpId 企业ID
-     * @param array $filter 筛选条件
+     * @param array $data 筛选条件
      * @return array
      */
-    public function list(int $corpId, array $filter = []): array;
+    public function list(array $data = []): array;
     
     /**
      * 员工离职处理
-     * @param int $corpId 企业ID
      * @param int $employeeId 员工ID
      * @param string $reason 离职原因
      * @return array
      */
-    public function resign(int $corpId, int $employeeId, string $reason = ''): array;
+    public function resign(int $employeeId, string $reason = ''): array;
 } 
